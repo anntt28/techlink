1cab49b0155b28e3c9186fcdce0e000954fb7700
anntt <anntt>
2023-12-25 01:02:18 +0700

    homepage 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,215 +1,67 @@
 $(function () {
- $(".play-video-slider").click(function () {
-  $(this).siblings('.video-slider').get(0).play();
-  $(this).addClass('active');
- });
- var swiper2 = new Swiper(".product-category-h .slider-tab-p", {
-  loop: true,
-  loopedSlides: 1,
-  slidesPerView: 2,
-  slidesPerGroup: 1,
-  spaceBetween: 5,
-  freeMode: true,
-  watchSlidesProgress: true,
-  spaceBetween: 10,
-  slideToClickedSlide: true,
-  navigation: {
-   nextEl: ".swiper-button-next-1",
-   prevEl: ".swiper-button-prev-1",
-  },
-  breakpoints: {
-    470: {
-      slidesPerView: 3,
-      spaceBetween: 8,
-    },
-    768: {
-      slidesPerView: 5,
-      spaceBetween: 8,
-    },
-    1200: {
-      slidesPerView: 7,
-      spaceBetween: 8,
-    },
-  },
- });
- var swiper3 = new Swiper(".product-category-h .slider-product-p", {
-  slidesPerView: 1,
-  slidesPerGroup: 1,
-  spaceBetween: 24,
-  navigation: {
-   nextEl: ".swiper-button-next-2",
-   prevEl: ".swiper-button-prev-2",
-  },
-  pagination: {
-    el: ".swiper-pagination-2",
-    clickable: true,
-   },
-  thumbs: {
-   swiper: swiper2,
-  },
- });
- var swiper4 = new Swiper(".slider-review-customer", {
+ var swiper1 = new Swiper(".slider-banner-top", {
   loop: true,
   slidesPerView: 1,
   spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next-1",
+    prevEl: ".swiper-button-prev-1",
+  },
+ });
+ var swiper2 = new Swiper(".slider-banner-multi", {
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next-2",
+    prevEl: ".swiper-button-prev-2",
+  },
+ });
+ var swiper3 = new Swiper(".slider-category-product", {
+  slidesPerView: 8,
+  spaceBetween: 0,
+  slidesPerColumn: 3,
+  navigation: {
+    nextEl: ".swiper-button-next-3",
+    prevEl: ".swiper-button-prev-3",
+  },
+  grid: {
+    rows: 2,
+  },
+ });
+ var swiper4 = new Swiper(".slider-banner-pr-1", {
+  loop: true,
+  slidesPerView: 1,
   navigation: {
     nextEl: ".swiper-button-next-4",
     prevEl: ".swiper-button-prev-4",
   },
-  pagination: {
-   el: ".swiper-pagination-1",
-   clickable: true,
-  },
-  breakpoints: {
-    1024: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-    },
-    576: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-    },
-  },
  });
- var swiper5 = new Swiper(".slider-blog-news", {
+ var swiper5 = new Swiper(".slider-category-text", {
   loop: true,
-  slidesPerView: 1,
-  spaceBetween: 30,
-  breakpoints: {
-    768: {
-        slidesPerView: 2,
-        spaceBetween: 30,
-    },
-  },
- });
- var swiper6 = new Swiper(".product-hataco-slider", {
-  cssMode: false,
-  loopedSlides: 1, 
-  slideToClickedSlide: true,
-  paginationClickable: true,
-  centeredSlides: true,
-  centeredSlidesBounds: true,
-  slidesPerView: 3,
-  slidesPerGroup: 1,
-  spaceBetween: 30,
+  spaceBetween: 5,
+  slidesPerView: 8,
   navigation: {
-   nextEl: ".swiper-button-next",
-   prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination-3",
-    clickable: true,
-   },
-  breakpoints: {
-    768: {
-      slidesPerView: 4.1,
-      centeredSlides: false,
-      slidesOffsetBefore:15,
-    },
-    1200:{
-      centeredSlides: false,
-      slidesOffsetBefore:15,
-      slidesOffsetAfter:15,
-      slidesPerView: 6.1,
-      spaceBetween: 50,
-    },
+    nextEl: ".swiper-button-next-5",
+    prevEl: ".swiper-button-prev-5",
   },
  });
- var swiper7 = new Swiper(".slider-video-hataco-pc", {
-  initialSlide: 3,
-  cssMode: false,
-  paginationClickable: true,
-  slidesPerView: 2,
-  spaceBetween: 30,
-  navigation: {
-   nextEl: ".swiper-button-next-3",
-   prevEl: ".swiper-button-prev-3",
-  }
- });
- var swiper8 = new Swiper(".slider-thumb-video", {
-  loop:true,
-  slidesPerView: 3,
-  slidesPerGroup: 1,
-  freeMode: true,
-  watchSlidesProgress: true,
-  spaceBetween: 10,
-  slideToClickedSlide: true,
-  navigation: {
-   nextEl: ".swiper-button-next-5",
-   prevEl: ".swiper-button-prev-5",
-  }
- });
- var swiper9 = new Swiper(".slider-video-hataco-mobile", {
-  slidesPerView: 1,
-  slidesPerGroup: 1,
-  spaceBetween: 10,
-  thumbs: {
-   swiper: swiper8,
-  },
- });
- var swiper10 = new Swiper(".product-gallery-h .slider-thumb-child", {
-  loop:false,
-  slidesPerView: 3,
-  slidesPerGroup: 1,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
-  spaceBetween: 10,
+ var swiper6 = new Swiper(".slider-category-text-2", {
+  loop: true,
+  spaceBetween: 5,
+  slidesPerView: 8,
   navigation: {
     nextEl: ".swiper-button-next-6",
     prevEl: ".swiper-button-prev-6",
-   },
-   breakpoints: {
-    1200:{
-      slidesPerView: 4,
-      spaceBetween: 21,
-    },
   },
  });
- var swiper11 = new Swiper(".product-gallery-h .slider-images-main", {
-  slidesPerView: 1,
-  slidesPerGroup: 1,
-  spaceBetween: 24,
-  thumbs: {
-   swiper: swiper10,
-  },
- });
- var swiper12 = new Swiper(".slider-product-child-a", {
-  slidesPerView: 2,
-  slidesPerGroup: 1,
-  spaceBetween: 15,
+ var swiper7 = new Swiper(".slider-banner-three", {
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 3,
   navigation: {
-   nextEl: ".swiper-button-next-7",
-   prevEl: ".swiper-button-prev-7",
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 25,
-    },
-    1200:{
-      slidesPerView: 5,
-      spaceBetween: 25,
-    },
-  },
- });
- var swiper13 = new Swiper(".slider-product-child-b", {
-  slidesPerView: 2,
-  slidesPerGroup: 1,
-  spaceBetween: 15,
-  navigation: {
-   nextEl: ".swiper-button-next-8",
-   prevEl: ".swiper-button-prev-8",
-  },
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 25,
-    },
-    1200:{
-      slidesPerView: 5,
-      spaceBetween: 25,
-    },
+    nextEl: ".swiper-button-next-7",
+    prevEl: ".swiper-button-prev-7",
   },
  });
  $('.content-page-cate-pr .woocommerce-ordering input[type="radio"]').each(function(index) {
